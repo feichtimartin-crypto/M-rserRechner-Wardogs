527035381ff8c52bfe15797687491e4af025c287
feichtimartin-crypto <feichti.martin@googlemail.com>
2026-09-14 17:56:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -134,14 +134,14 @@ st.write(
 col1, col2 = st.columns(2)
 
 with col1:
-    st.markdown("**🎯 Ziel**")
-    x_ziel = st.number_input("X Ziel", value=0.0, format="%.2f", key="x_ziel")
-    y_ziel = st.number_input("Y Ziel", value=0.0, format="%.2f", key="y_ziel")
-
-with col2:
     st.markdown("**💣 Mörser**")
     x_moerser = st.number_input("X Mörser", value=0.0, format="%.2f", key="x_moerser")
     y_moerser = st.number_input("Y Mörser", value=0.0, format="%.2f", key="y_moerser")
+
+with col2:
+    st.markdown("**🎯 Ziel**")
+    x_ziel = st.number_input("X Ziel", value=0.0, format="%.2f", key="x_ziel")
+    y_ziel = st.number_input("Y Ziel", value=0.0, format="%.2f", key="y_ziel")
 
 # ------------------------------------------------------------
 # Berechnung
